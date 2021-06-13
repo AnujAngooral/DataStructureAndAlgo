fc160c1187894b44eee3ec58aaa0e4bec09ef556
Anuj Angooral <anujangooral@gmail.com>
2021-06-13 16:35:53 +0530

    Reverse Linked List

--- Top Interview Questions/LinkedListProblems.cs
+++ Top Interview Questions/LinkedListProblems.cs
@@ -22,9 +22,9 @@ namespace Top_Interview_Questions
         /// <returns></returns>
         public static ListNode RemoveNthFromEnd(ListNode head, int n)
         {
-         // We have added the dummy node to handle below use case:
-        //Input: head = [1], n = 1
-        //    Output:[]
+            // We have added the dummy node to handle below use case:
+            //Input: head = [1], n = 1
+            //    Output:[]
 
             ListNode dummy = new ListNode(0);
             dummy.next = head;
@@ -32,10 +32,10 @@ namespace Top_Interview_Questions
             ListNode first = dummy;
             ListNode second = dummy;
 
-            for (int i = 0; i < n+1; i++)
+            for (int i = 0; i < n + 1; i++)
             {
                 first = first.next;
-               
+
             }
 
             while (first != null)
@@ -46,6 +46,30 @@ namespace Top_Interview_Questions
 
             second.next = second.next.next;
             return dummy.next;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="head"></param>
+        /// <returns></returns>
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            ListNode first = head;
+            ListNode second = head.next;
+            ListNode third = head;
+            while (second != null)
+            {
+                third = second.next;
+                second.next = first;
+                first = second;
+                second = third;
+            }
+            head.next = null;
+            head = first;
+            return head;
         }
     }
 }
