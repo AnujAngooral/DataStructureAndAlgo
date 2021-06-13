111ae6f79d4f82fd719d63a3e4dd2e06be948e93
Anuj Angooral <anujangooral@gmail.com>
2021-06-13 16:11:01 +0530

    RemoveNthFromEnd

--- Top Interview Questions/LinkedListProblems.cs
+++ Top Interview Questions/LinkedListProblems.cs
@@ -4,15 +4,48 @@ using System.Text;
 
 namespace Top_Interview_Questions
 {
-    public class LinkedListProblems
+    public static class LinkedListProblems
     {
-        public void DeleteNode(ListNode node)
+        public static void DeleteNode(ListNode node)
         {
             if (node.next != null)
             {
                 node.val = node.next.val;
                 node.next = node.next.next;
             }
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="head"></param>
+        /// <param name="n"></param>
+        /// <returns></returns>
+        public static ListNode RemoveNthFromEnd(ListNode head, int n)
+        {
+         // We have added the dummy node to handle below use case:
+        //Input: head = [1], n = 1
+        //    Output:[]
+
+            ListNode dummy = new ListNode(0);
+            dummy.next = head;
+
+            ListNode first = dummy;
+            ListNode second = dummy;
+
+            for (int i = 0; i < n+1; i++)
+            {
+                first = first.next;
+               
+            }
+
+            while (first != null)
+            {
+                first = first.next;
+                second = second.next;
+            }
+
+            second.next = second.next.next;
+            return dummy.next;
         }
     }
 }
--- Top Interview Questions/Program.cs
+++ Top Interview Questions/Program.cs
@@ -6,6 +6,21 @@ namespace Top_Interview_Questions
     {
         static void Main(string[] args)
         {
+            ListNode node = new ListNode(1);
+
+            //var node2 = new ListNode(2);
+            //node.next = node2;
+
+            //var node3 = new ListNode(3);
+            //node2.next = node3;
+
+            //var node4 = new ListNode(4);
+            //node3.next = node4;
+
+            //var node5 = new ListNode(5);
+            //node4.next = node5;
+
+            LinkedListProblems.RemoveNthFromEnd(node, 1);
             Console.WriteLine("Hello World!");
         }
     }
