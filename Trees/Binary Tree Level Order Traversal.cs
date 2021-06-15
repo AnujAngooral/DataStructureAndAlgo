d38e342f0c4a67006d98d8a4b00c8eb5cce836c0
Anuj Angooral <anujangooral@gmail.com>
2021-06-15 20:51:45 +0530

    Binary_Tree_Level_Order_Traversal

---
+++ Trees/Binary Tree Level Order Traversal.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class Binary_Tree_Level_Order_Traversal
+    {
+        List<IList<int>> answer = new List<IList<int>>();
+        Queue<TreeNode> que = new Queue<TreeNode>();
+        public IList<IList<int>> LevelOrder(TreeNode root)
+        {
+
+            helper(root, 0);
+            return answer;
+        }
+
+        void helper(TreeNode root, int level)
+        {
+            que.Enqueue(root);
+
+            while (que.Count > 0)
+            {
+                List<int> subList = new List<int>();
+                for (int i = 0; i < level + 1; i++)
+                {
+                    var currentNode = que.Dequeue();
+                    subList.Add(currentNode.val);
+
+                    if (currentNode.left != null)
+                        que.Enqueue(currentNode.left);
+
+                    if (currentNode.right != null)
+                        que.Enqueue(currentNode.right);
+                }
+                level++;
+            }
+        }
+    }
+}
