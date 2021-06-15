80fcaf3e5e32a98b35dcb3a44087585bf0fd443d
Anuj Angooral <anujangooral@gmail.com>
2021-06-15 08:37:01 +0530

    Binary_Tree_Preorder_Traversal

---
+++ Top Interview Questions/Trees.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Top_Interview_Questions
+{
+    public class Trees
+    {
+            
+    }
+}
---
+++ Trees/Binary Tree Preorder Traversal.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class Binary_Tree_Preorder_Traversal
+    {
+        IList<int> lst = new List<int>();
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+
+            if (root != null)
+                helper(root);
+
+            return lst;
+        }
+
+        private void helper(TreeNode root)
+        {
+
+            lst.Add(root.val);
+
+            if (root.left != null)
+                helper(root.left);
+            if (root.right != null)
+                helper(root.right);
+
+        }
+    }
+}
