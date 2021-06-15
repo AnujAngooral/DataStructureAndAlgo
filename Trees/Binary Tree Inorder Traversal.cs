90810a411ce7624cebec8a8f173c895d775fcbc7
Anuj Angooral <anujangooral@gmail.com>
2021-06-15 09:27:51 +0530

    Binary_Tree_Inorder_Traversal

---
+++ Trees/Binary Tree Inorder Traversal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    public class Binary_Tree_Inorder_Traversal
+    {
+        IList<int> lst = new List<int>();
+        public IList<int> InorderTraversal(TreeNode root)
+        {
+
+            if (root != null)
+                helper(root);
+
+            return lst;
+        }
+
+        private void helper(TreeNode root)
+        {
+            if (root.left != null)
+                helper(root.left);
+
+            lst.Add(root.val);
+            if (root.right != null)
+                helper(root.right);
+
+        }
+    }
+}
