67653c1a65671543c2cc8336e6105eafbc0c4887
Anuj Angooral <anujangooral@gmail.com>
2021-06-15 10:05:53 +0530

    Post Order

---
+++ Trees/Binary Tree Postorder Traversal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Trees
+{
+    class Binary_Tree_Postorder_Traversal
+    {
+        IList<int> lst = new List<int>();
+        public IList<int> PostorderTraversal(TreeNode root)
+        {
+
+            if (root != null)
+                helper(root);
+            return lst;
+        }
+
+        private void helper(TreeNode root)
+        {
+
+            if (root.left != null)
+                helper(root.left);
+            if (root.right != null)
+                helper(root.right);
+
+            lst.Add(root.val);
+
+        }
+    }
+}
