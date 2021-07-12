f3de362b7ae2e4e09d7ebf09f9faeb9766a92132
Anuj Angooral <anujangooral@gmail.com>
2021-07-12 23:01:58 +0530

    Squares of a Sorted Array
    Squares of a Sorted Array

---
+++ Arrays/Find Numbers with Even Number of Digits.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class Find_Numbers_with_Even_Number_of_Digits
+    {
+        public int FindNumbers(int[] nums)
+        {
+            if (nums.Length == 0) return 0;
+            int result = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (NumberOfDigits(nums[i]) % 2 == 0)
+                    result++;
+            }
+
+            return result;
+        }
+
+        int NumberOfDigits(int number)
+        {
+            int digits = 0;
+            while (number != 0)
+            {
+                number /= 10;
+                digits++;
+            }
+
+            return digits;
+        }
+    }
+
+
+}
---
+++ Arrays/Squares of a Sorted Array.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class Squares_of_a_Sorted_Array
+    {
+        public int[] SortedSquares(int[] A)
+        {
+            //Input: nums = [-4, -1, 0, 3, 10]
+            //Output:[0,1,9,16,100]
+
+            int[] sortedSquares = new int[A.Length];
+            int i = 0;
+            int j = A.Length - 1;
+            int k = j;
+            while (i < j)
+            {
+                int start = A[i] * A[i];
+                int end = A[j] * A[j];
+
+                sortedSquares[k--] = Math.Max(start, end);
+
+                if (start > end)
+                    i++;
+                else j--;
+            }
+            return sortedSquares;
+        }
+    }
+}
