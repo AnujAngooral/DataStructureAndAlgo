26870d3dccd9ef18e77c2e9394b73a8f340f2f8b
Anuj Angooral <anujangooral@gmail.com>
2021-07-12 23:22:13 +0530

    Duplicate Zeros

---
+++ Arrays/Duplicate Zeros.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    public class Duplicate_Zeros
+    {
+        public void DuplicateZeros(int[] arr)
+        {
+            if (arr == null || arr.Length == 0) return;
+            int count = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] == 0) { count++; break; }
+
+            }
+
+            if (count > 0)
+            {
+
+                for (int i = 0; i < arr.Length; i++)
+                {
+                    if (arr[i] == 0)
+                    {
+
+                        for (int j = arr.Length - 1; j > i; j++)
+                        {
+
+                        }
+                    }
+                }
+
+            }
+        }
+    }
+}
