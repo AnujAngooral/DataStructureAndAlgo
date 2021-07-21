11a1eb6d87bfc344c899172a7408c2f003e2aaed
Anuj Angooral <anujangooral@gmail.com>
2021-07-21 18:12:56 +0530

    Replace Elements with Greatest Element on Right Side

---
+++ Arrays/Replace Elements with Greatest Element on Right Side.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Arrays
+{
+    class Replace_Elements_with_Greatest_Element_on_Right_Side
+    {
+        public int[] ReplaceElements(int[] arr)
+        {
+            if (arr.Length == 1)
+            {
+                arr[0] = -1;
+                return arr;
+            }
+
+            int max = -1;
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                int temp = max;
+                max = Math.Max(max,arr[i]);
+                arr[i] = temp;
+            }
+
+            return arr;
+        }
+    }
+}
