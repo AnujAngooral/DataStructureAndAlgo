cd945c59ae215756012b0fef63dd176bf49cfdcd
Anuj Angooral <anujangooral@gmail.com>
2021-07-24 00:32:08 +0530

    Reverse Words in a String II

--- Microsoft/Program.cs
+++ Microsoft/Program.cs
@@ -6,8 +6,8 @@ namespace Microsoft
     {
         static void Main(string[] args)
         {
-            Reverse_Words_in_a_String.ReverseWords("the sky is blue");
-            //Valid_Palindrome.IsPalindrome("A man, a plan, a canal: Panama");
+            char[] s = new char[] { 't', 'h', 'e', ' ', 's', 'k', 'y', ' ', 'i', 's', ' ', 'b', 'l', 'u', 'e'}; 
+            Reverse_Words_in_a_String_II.ReverseWords(s);
             Console.WriteLine("Hello World!");
         }
     }
---
+++ Microsoft/Reverse Words in a String II.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft
+{
+  public static class Reverse_Words_in_a_String_II
+    {
+        public static void ReverseWords(char[] s)
+        {
+            s.reverse(0, s.Length - 1);
+            int i = 0;
+            while (i < s.Length) {
+                int left = i;
+                while (i<s.Length && s[i] != ' ') i++;
+
+                s.reverse(left, i-1);
+                i++;
+            }
+        }
+
+        static void reverse(this char[] s, int left,int right)
+        {
+
+            while (left<right)
+            {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+
+        }
+    }
+}
