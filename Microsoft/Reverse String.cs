781e55abcdf8c3c80be04f9e428b86710eb9cdfb
Anuj Angooral <anujangooral@gmail.com>
2021-07-23 21:01:51 +0530

    Reverse String

---
+++ Microsoft/Reverse String.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Microsoft
+{
+    public class Reverse_String
+    {
+        public void ReverseString(char[] s)
+        {
+            //Input: s = ["h", "e", "l", "l", "o"]
+            //Output:["o","l","l","e","h"]
+
+            int left = 0;
+            int right = s.Length - 1;
+
+            while (left < right)
+            {
+                char temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;right--;
+            }
+
+        }
+    }
+}
